5c99924ef3f226be7b7742d1c7987aee8c6d2b39
tls427wodnr <tlswodnr427@naver.com>
2026-05-06 15:23:02 +0900

    Chore - 테이블 이름 parks 에서 park로 수정

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/map/entity/Park.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/map/entity/Park.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "parks")
+@Table(name = "park")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
