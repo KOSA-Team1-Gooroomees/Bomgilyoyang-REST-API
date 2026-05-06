7325c54094751bdbb3e83179d9a41ae960f38981
tls427wodnr <tlswodnr427@naver.com>
2026-05-06 11:51:14 +0900

    Setting - 프로젝트 초기 설정

---
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/NeulbomgilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gooroomees.neulbomgil_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeulbomgilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeulbomgilBackendApplication.class, args);
+	}
+
+}
---
+++ neulbomgil-backend/src/test/java/com/gooroomees/neulbomgil_backend/NeulbomgilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gooroomees.neulbomgil_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeulbomgilBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
