e84d7fe48bdcdeb78acae01f8d967b3cda2491b9
yunji cho <rlarn2003@naver.com>
2026-05-07 14:11:25 +0900

    feat#14-board entity, dto

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/board/dto/BoardResponseDTO.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/board/dto/BoardResponseDTO.java
@@ -1,4 +1,23 @@
 package com.gooroomees.neulbomgil_backend.domain.board.dto;
+import com.gooroomees.neulbomgil_backend.domain.board.entity.Board;
+import lombok.Getter;
 
+import java.time.LocalDateTime;
+@Getter
 public class BoardResponseDTO {
+    private int boardid;
+    private String userid;
+    private String title;
+    private String content;
+    private int cnt;
+    private LocalDateTime writedate;
+
+    public BoardResponseDTO(Board board) {
+        this.boardid = board.getBoardid();
+        this.userid = board.getUserid();
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.cnt = board.getCnt();
+        this.writedate = board.getWritedate();
+    }
 }
