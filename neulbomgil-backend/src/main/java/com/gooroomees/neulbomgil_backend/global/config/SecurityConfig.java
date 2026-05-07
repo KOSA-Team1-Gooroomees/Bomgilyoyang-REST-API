0d8993fb248e5c0b804f3b99d13a1c6037f00c90
minho2618 <minho2618@naver.com>
2026-05-07 14:14:18 +0900

    Feat#10 - SecurityConfig 생성

---
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/global/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.gooroomees.neulbomgil_backend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity // 웹 보안 활성화, 이 어노테이션이 붙은 클래스는 스프링 시큐리티의 구성 클래스로 사용된다.
+@RequiredArgsConstructor
+@EnableMethodSecurity // 자바 메소드 단위에서 보안 설정을 적용할 때 사용하는 어노테이션
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    // private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/api/auth").permitAll()
+                        //.requestMatchers()
+                        .anyRequest().authenticated()
+                )
+                //.authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
