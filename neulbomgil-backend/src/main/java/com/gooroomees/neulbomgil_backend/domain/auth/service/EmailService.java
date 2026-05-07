f44f2dd9663cb9f1377b4bd12c137fd6020f6d58
minho2618 <minho2618@naver.com>
2026-05-07 13:18:33 +0900

    Feat#10 - EmailService 생성

---
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/service/EmailService.java
@@ -0,0 +1,21 @@
+package com.gooroomees.neulbomgil_backend.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@example.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
