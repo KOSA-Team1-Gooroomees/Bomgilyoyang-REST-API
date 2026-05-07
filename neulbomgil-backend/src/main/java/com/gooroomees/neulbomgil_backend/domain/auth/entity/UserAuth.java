e27598b6c92e3475819ccd547caf5fabbbbf50fc
minho2618 <minho2618@naver.com>
2026-05-07 15:05:05 +0900

    Feat#10 - UserAuth에 UserDetails 인터페이스 구현

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/entity/UserAuth.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/entity/UserAuth.java
@@ -6,18 +6,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserAuth {
+public class UserAuth implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
     @Column(nullable = false)
@@ -34,5 +39,40 @@ public class UserAuth {
     private LocalDateTime createdAt;
 
     private LocalDateTime modifiedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return status.equals(Status.ACTIVE);
+    }
 
 }
