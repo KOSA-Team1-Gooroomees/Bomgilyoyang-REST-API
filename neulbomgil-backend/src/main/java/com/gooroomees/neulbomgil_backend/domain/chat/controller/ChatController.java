269b7f52281fdae189e42f7a2879769f3d4a62ac
DESKTOP-68N9GVG\KOSA <dnwldus410@naver.com>
2026-05-12 15:02:15 +0900

    feat/#105 - JWT 토큰 기반 사용자 식별 적용

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/chat/controller/ChatController.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/chat/controller/ChatController.java
@@ -1,10 +1,12 @@
 package com.gooroomees.neulbomgil_backend.domain.chat.controller;
 
+import com.gooroomees.neulbomgil_backend.domain.auth.entity.UserAuth;
 import com.gooroomees.neulbomgil_backend.domain.chat.dto.ChatRequestDto;
 import com.gooroomees.neulbomgil_backend.domain.chat.dto.ChatResponseDto;
 import com.gooroomees.neulbomgil_backend.domain.chat.dto.ChatRoomResponseDto;
 import com.gooroomees.neulbomgil_backend.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +19,8 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping("/start")
-    public ChatRoomResponseDto startChatRoom(@RequestParam Long userId) {
-        return chatService.startChatRoom(userId);
+    public ChatRoomResponseDto startChatRoom(@AuthenticationPrincipal UserAuth userAuth) {
+        return chatService.startChatRoom(userAuth.getUserId());
     }
 
     @GetMapping("/{roomId}/message")
@@ -37,13 +39,13 @@ public class ChatController {
     }
 
     @PutMapping("/{roomId}/read")
-    public void readMessages(@PathVariable Long roomId, @RequestParam Long userId) {
-        chatService.readMessages(roomId, userId);
+    public void readMessages(@PathVariable Long roomId, @AuthenticationPrincipal UserAuth userAuth) {
+        chatService.readMessages(roomId, userAuth.getUserId());
     }
 
     @GetMapping("/unread")
-    public boolean hasUnreadChats(@RequestParam Long userId) {
-       return chatService.hasUnreadChats(userId);
+    public boolean hasUnreadChats(@AuthenticationPrincipal UserAuth userAuth) {
+       return chatService.hasUnreadChats(userAuth.getUserId());
     }
 
 }
