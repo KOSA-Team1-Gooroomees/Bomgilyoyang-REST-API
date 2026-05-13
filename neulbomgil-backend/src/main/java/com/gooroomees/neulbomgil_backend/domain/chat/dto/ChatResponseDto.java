255b033ca544b6e5bb3970c9b1a7bf6275a154ac
DESKTOP-68N9GVG\KOSA <dnwldus410@naver.com>
2026-05-13 10:50:26 +0900

    refactor/#128 - chatService의 N+1문제 해결 및 사용안하는 import삭제

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/chat/dto/ChatResponseDto.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/chat/dto/ChatResponseDto.java
@@ -1,8 +1,5 @@
 package com.gooroomees.neulbomgil_backend.domain.chat.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/chat/repository/ChatRoomRepository.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/chat/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.gooroomees.neulbomgil_backend.domain.chat.repository;
 
 
 import com.gooroomees.neulbomgil_backend.domain.auth.entity.UserAuth;
+import com.gooroomees.neulbomgil_backend.domain.chat.dto.ChatRoomResponseDto;
 import com.gooroomees.neulbomgil_backend.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -27,6 +28,27 @@ select cr from ChatRoom cr where cr.roomId = :roomId
 
     )
     Optional<ChatRoom> findById(Long roomId);
+
+    @Query("""
+    select new com.gooroomees.neulbomgil_backend.domain.chat.dto.ChatRoomResponseDto(
+        cr.roomId,
+        u.userId,
+        u.name,
+        c.message,
+        cr.lastMessageAt
+    )
+    from ChatRoom cr
+    join cr.user u
+    left join Chat c
+        on c.chatRoom = cr
+       and c.createdAt = (
+            select max(c2.createdAt)
+            from Chat c2
+            where c2.chatRoom = cr
+       )
+    order by cr.lastMessageAt desc
+""")
+    List<ChatRoomResponseDto> findAllChatRoomResponses();
 
 
 }
