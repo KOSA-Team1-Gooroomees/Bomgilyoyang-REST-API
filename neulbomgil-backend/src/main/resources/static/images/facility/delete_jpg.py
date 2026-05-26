09b1afb116fcdc064993dd53bed642270a2191d6
dnwldus410 <dnwldus410@naver.com>
2026-05-26 14:42:36 +0900

    feat/#156 - 관리자페이지 타임리프

--- neulbomgil-backend/src/main/resources/static/images/facility/delete_jpg.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-current_dir = Path(".")
-
-for file in current_dir.iterdir():
-
-    if file.suffix.lower() in [".png", ".jpeg"]:
-        file.unlink()
-        print(f"삭제 완료: {file.name}")
