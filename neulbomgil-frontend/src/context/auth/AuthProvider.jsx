e3bb2c51d6dd16560fa6c5cda50f4add428e97fd
tls427wodnr <tlswodnr427@naver.com>
2026-06-01 17:53:42 +0900

    feat#25 - AuthContext,AuthProvider,useAuth 추가

---
+++ neulbomgil-frontend/src/context/auth/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import { AuthContext} from "./AuthContext.jsx";
+import {useState} from "react";
+
+export const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const login = () => setIsLoggedIn(true);
+    const logout = () => setIsLoggedIn(false);
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
